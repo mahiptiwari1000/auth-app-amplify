d3d760591ee86417c7d7765c954e50970575a249
Mahip Tiwari <mahiptiwari@Mahips-MacBook-Air.local>
2024-11-17 18:08:36 -0600

    Initial Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Logout from "@/components/Logout";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Logout/>
+      <h1>Hello Amplify Auth</h1>
+    </div>
+  );
+}
